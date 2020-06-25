e1fb37038d27a481fc6f298f72dcbb9ff822f1a8
initinfosec <initinfosec@protonmail.com>
2020-06-25 12:50:42 -0500

    minor updates

--- setup.sh
+++ setup.sh
@@ -185,7 +185,7 @@ done
 printf '\n%.s' {1..3}
 printf '============================================================================'
 printf '\n%.s' {1..3}
-echo -e "AutoRecon by Tib3rius installed! (https://github.com/Tib3rius/AutoRecon)\n"
+echo -e "AutoRecon by Tib3rius installed!   more info at: https://github.com/Tib3rius/AutoRecon\n"
 echo -e "install script/wrapper by @initinfosec\n"
 echo -e "'It's like bowling with bumpers.' - @ippsec\n\n"
 printf '============================================================================'
